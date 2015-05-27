6ff8a65de6ebf41efdc8b75e4a67fe5b900f89fd
dikirilov <dikirilovskiy@gmail.com>
2015-05-27 18:40:11 +0300

    fixed problems, trying traffic

--- jobs/zabbix_trigger.rb
+++ jobs/zabbix_trigger.rb
@@ -17,7 +17,7 @@ require 'active_support/core_ext/numeric/time'
   lav_hgh = 0
   lav_dis = 0
 
-SCHEDULER.every '10s' do
+SCHEDULER.every '1s' do
 
   serv = Zabby.init do
     set :server => "http://zabbix.guu.ru/zabbix"
@@ -26,27 +26,25 @@ SCHEDULER.every '10s' do
     login
   end
 
-  env = serv.run { Zabby::Trigger.get "filter" => { "priority" => [ 2, 3, 4, 5 ] }, "output" => "extend", "only_true" => "true", "monitored" => 1, "withUnacknowledgedEvents" => 1, "skipDependent" => 1, "expandData" => "host" } 
-  
+  env = serv.run { Zabby::Trigger.get "filter" => { "priority" => [ 4, 5 ] }, "output" => "extend", "only_true" => "true", "monitored" => 1, "withUnacknowledgedEvents" => 1, "skipDependent" => 1, "expandData" => "host" } 
   hosts_req = serv.run { Zabby::Host.get "monitored_hosts" => 1, "output" => "extend"}
-
   temp1_req = serv.run { Zabby::Item.get "filter" => {"itemid" => "45443"}}
-
   temp2_req = serv.run { Zabby::Item.get "filter" => {"itemid" => "45444"}}
-
+  graph_req = serv.run { Zabby::Item.get "filter" => {"itemid" => ["30399", "30449"]}}
 
   hosts = JSON.parse(hosts_req.to_json)
-  pas = JSON.parse(env.to_json)
+  trig = JSON.parse(env.to_json)
   temp1 = JSON.parse(temp1_req.to_json)
   temp2 = JSON.parse(temp2_req.to_json)  
+  graph = JSON.parse(graph_req.to_json)
 
-#p pas
+#p trig
 
-  pas.each do |res|
-    if (res["priority"] == 4 || res["priority"] == 5)
-        sdis << res["hostname"]
-      end
-  end
+#  pas.each do |res|
+#    if (res["priority"] == 4 || res["priority"] == 5)
+#        sdis << res["hostname"]
+#      end
+#  end
 
 #  lav_warn = twarn
 #  lav_avrg = tavrg
@@ -56,20 +54,27 @@ SCHEDULER.every '10s' do
   warn = temp1[0]["lastvalue"] 
   avrg = temp2[0]["lastvalue"]
   hgh = hosts.count 
-  dis = sdis.count 
+  dis = trig.count 
+  gdata = [{ "x" => Time.now, "y" => graph[0]["lastvalue"]}, {"x" => Time.now, "y" => graph[1]["lastvalue"]}]
 
 #  warn = twarn - lav_warn 
 #  avrg = tavrg - lav_avrg
 #  hgh = thgh - lav_hgh 
 #  dis = tdis - lav_dis 
 
-  if warn.to_i > 0 then warnstats = "warn" else warnstats = "ok" end
-  if avrg.to_i > 0 then avrgstats = "average" else avrgstats = "ok" end
-  if hgh > 0 then hghstats = "high" else hghstats = "ok" end
+  if (warn.to_i > 28) then warnstats = "high" 
+  elsif (warn.to_i >25) then warnstats = "warn"
+  else warnstats = "ok" end
+  if (avrg.to_i > 28) then avrgstats = "high" 
+  elsif (avrg.to_i > 25) then avrgstatus = "warn"
+  else  avrgstats = "ok" end
+  #if hgh > 0 then hghstats = "high" else hghstats = "ok" end
+  hghstats = "ok"
   if dis > 0 then disstats = "disaster" else disstats = "ok" end
   send_event( 'outwarn', { current: warn, status: warnstats } )
   send_event( 'outavrg', { current: avrg, status: avrgstats } )
   send_event( 'outhigh', { current: hgh, status: hghstats  } )
   send_event( 'outdis', { current: dis, status: disstats  } )
-  
+  send_event( 'traffic', points: gdata ) 
+
 end

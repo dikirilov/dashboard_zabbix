8c24512bc658899229d54351f23de59572a498b1
dikirilov <dikirilovskiy@gmail.com>
2015-06-01 18:36:31 +0300

    Add clock

---
+++ jobs/hosts.rb
@@ -0,0 +1,25 @@
+require 'zabby'
+require 'json'
+require 'active_support/core_ext/numeric/time'
+
+SCHEDULER.every '1d' do
+
+  serv = Zabby.init do
+    set :server => "http://zabbix.guu.ru/zabbix"
+    set :user => "admin"
+    set :password => "guu_zabbix"
+    login
+  end
+
+  today = Time.now.strftime("%d.%m")
+
+  hosts_req = serv.run { Zabby::Host.get "monitored_hosts" => 1, "output" => "extend"}
+  hosts = JSON.parse(hosts_req.to_json)
+  hostc = hosts.count
+  send_event( 'amount', { current: hostc, status: "ok", updatedAt: today } )
+end
+
+
+
+
+
--- jobs/zabbix.rb
+++ jobs/zabbix.rb
@@ -2,7 +2,7 @@ require 'zabby'
 require 'json'
 require 'active_support/core_ext/numeric/time'
 
-SCHEDULER.every '3s' do
+SCHEDULER.every '1m' do
 
   serv = Zabby.init do
     set :server => "http://zabbix.guu.ru/zabbix"
@@ -12,13 +12,13 @@ SCHEDULER.every '3s' do
   end
 
   env = serv.run { Zabby::Trigger.get "filter" => { "priority" => [ 4, 5 ] }, "output" => "extend", "only_true" => "true", "monitored" => 1, "withUnacknowledgedEvents" => 1, "skipDependent" => 1, "expandData" => "host" } 
-  hosts_req = serv.run { Zabby::Host.get "monitored_hosts" => 1, "output" => "extend"}
+#  hosts_req = serv.run { Zabby::Host.get "monitored_hosts" => 1, "output" => "extend"}
   temp1_req = serv.run { Zabby::Item.get "filter" => {"itemid" => "45443"}}
   temp2_req = serv.run { Zabby::Item.get "filter" => {"itemid" => "45444"}}
   graph_req = serv.run { Zabby::Item.get "filter" => {"itemid" => ["30399", "30449"]}}
   cpu6_req = serv.run { Zabby::Item.get "filter" => {"itemid" => "44016"} }
 
-  hosts = JSON.parse(hosts_req.to_json)
+#  hosts = JSON.parse(hosts_req.to_json)
   trig = JSON.parse(env.to_json)
   temp1 = JSON.parse(temp1_req.to_json)
   temp2 = JSON.parse(temp2_req.to_json)  
@@ -28,7 +28,7 @@ SCHEDULER.every '3s' do
   tempf = temp1[0]["lastvalue"]
   tempb = temp2[0]["lastvalue"]
   temp = (tempf.to_i>tempb.to_i)? tempf : tempb
-  hostc = hosts.count 
+#  hostc = hosts.count 
   probs = trig.count 
   gdata = [{ "x" => Time.now, "y" => graph[0]["lastvalue"]}, {"x" => Time.now, "y" => graph[1]["lastvalue"]}]
 
@@ -43,13 +43,13 @@ SCHEDULER.every '3s' do
 
   if probs > 0 then probstats = "high" else disstats = "ok" end
   time = Time.now.strftime("%H:%M")
-  temp += "°"
-  cpu6 += "%"
+#  temp += "°"
+#  cpu6 += "%"
 
-  send_event( 'temp', { current: temp, status: tempstats, updatedAt: time } )
-  send_event( 'amount', { current: hostc, status: "ok", updatedAt: time } )
+  send_event( 'temp', { current: temp, status: tempstats, updatedAt: time, suffix: "°" } )
+#  send_event( 'amount', { current: hostc, status: "ok", updatedAt: time } )
   send_event( 'problems', { current: probs, status: probstats, updatedAt: time } )
   send_event( 'traffic', points: gdata ) 
-  send_event( 'cpu6', {current: cpu6, status: "ok", updatedAt: time  } )
+  send_event( 'cpu6', {current: cpu6, status: "ok", updatedAt: time, suffix: "%"  } )
 
 end

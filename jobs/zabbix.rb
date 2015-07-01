44a15016bd025245e92e7bcd44f09b7b0d69577b
dikirilov <dikirilovskiy@gmail.com>
2015-07-01 19:10:06 +0300

    Temps is done, need some visual with traffic

--- jobs/zabbix.rb
+++ jobs/zabbix.rb
@@ -27,14 +27,18 @@ SCHEDULER.every '20s' do
 
   tempf = temp1[0]["lastvalue"]
   tempb = temp2[0]["lastvalue"]
-  temp = (tempf.to_i>tempb.to_i)? tempf : tempb
+#  temp = (tempf.to_i>tempb.to_i)? tempf : tempb
   hostc = hosts.count 
   probs = trig.count 
-  gdata = [{ "x" => Time.now, "y" => graph[0]["lastvalue"]}, {"x" => Time.now, "y" => graph[1]["lastvalue"]}]
+#  gdata = [{ "x" => Time.now, "y" => graph[0]["lastvalue"]}, {"x" => Time.now, "y" => graph[1]["lastvalue"]}]
 
-  if (temp.to_i > 28) then tempstats = "high" 
-  elsif (temp.to_i >25) then tempstats = "warn"
-  else tempstats = "ok" end
+  if (tempb.to_i > 31) then status2 = "high" 
+  elsif (tempb.to_i >28) then status2 = "warn"
+  else status2 = "ok" end
+
+  if (tempf.to_i > 31) then status1 = "high"
+  elsif (tempf.to_i > 28) then status1 = "warn"
+  else status1 = "ok" end
 
   if probs > 0 then probstats = "high" else probstats = "ok" end
 
@@ -43,10 +47,15 @@ SCHEDULER.every '20s' do
   else cpu6stats = "ok" end
   time = Time.now.strftime("%H:%M")
 
-  send_event( 'temp', { current: temp, status: tempstats, updatedAt: time, suffix: "°" } )
+  download = (graph[0]["lastvalue"].to_i/1000000.0).to_s
+  upload = (graph[1]["lastvalue"].to_i/1000000.0).to_s
+
+  send_event( 'temps', { temp1: tempf, temp2: tempb, updatedAt: time, suffix: "°", status1: status1, status2: status2 })
+#  send_event( 'temp', { current: temp, status: tempstats, updatedAt: time, suffix: "°" } )
   send_event( 'amount', { current: hostc, updatedAt: time } )
   send_event( 'problems', { current: probs, status: probstats, updatedAt: time } )
-  send_event( 'traffic', points: gdata ) 
+  send_event( 'traff', value1: upload[0..4], value2: download[0..4], subtitle1: "Входящий", subtitle2: "Исходящий", updatedAt: time, fromInfo: "Zabbix" )
+#  send_event( 'download', value: download[0..4], updatedAt: time ) 
   send_event( 'cpu6', {current: cpu6, status: cpu6stats, updatedAt: time, suffix: "%"  } )
 
 end
---
+++ widgets/temps/temps.coffee
@@ -0,0 +1,27 @@
+class Dashing.Temps extends Dashing.Widget
+# @accessor 'temp1', Dashing.AnimatedValue
+# @accessor 'temp2', Dashing.AnimatedValue
+
+  ready: ->
+    # This is fired when the widget is done being rendered
+
+  onData: (data) ->
+    if data.status1
+#      $('#val1').attr 'class', (i,c) -> 
+#        c.replace /\bstatus-\S+\g, ''
+      $('#val1').addClass "status-#{data.status1}"
+    if data.status2
+#      $('#val2').attr 'class', (i,c) ->
+#        c.replace /\bstatus-\S+\g, ''
+      $('#val2').addClass "status-#{data.status2}"
+
+#    if data.status1
+#      alert(data.status2)
+#      val = $('$val1').val
+#      alert(val);
+#    alert($('#val1').val) #.attr 'class', (i,c) ->
+#      c.replace /\bstatus-\S+/g, ''
+#    $(@get('#val1')).addClass "status-#{data.status1}"
+    # Handle incoming data
+    # You can access the html node of this widget with `@node`
+    # Example: $(@node).fadeOut().fadeIn() will make the node flash each time data comes in.
